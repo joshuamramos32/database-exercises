2aa5325c6d5a8540883d74d4ad532d56334ba41d
Joshua Ramos <joshua.m.ramos32@gmail.com>
2023-01-25 16:06:09 -0600

    adding order_by_exercises

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -1,8 +1,13 @@
 USE employees;
 
-SELECT * FROM employees WHERE first_name IN('Irena', 'Vidya', 'Maya');
+SELECT * FROM employees WHERE first_name IN('Irena', 'Vidya', 'Maya') ORDER BY first_name;
 
-SELECT * FROM employees WHERE last_name LIKE 'E%';
+SELECT * FROM employees WHERE first_name IN('Irena', 'Vidya', 'Maya') ORDER BY first_name, last_name;
+
+SELECT * FROM employees WHERE first_name IN('Irena', 'Vidya', 'Maya') ORDER BY last_name, first_name;
+
+SELECT * FROM employees WHERE last_name LIKE '%E%' ORDER BY emp_no ASC;
+SELECT * FROM employees WHERE last_name LIKE '%E%' ORDER BY emp_no DESC;
 
 SELECT * FROM employees WHERE last_name LIKE '%q%';
 
@@ -15,6 +20,4 @@ SELECT * FROM employees WHERE last_name LIKE 'E%' OR last_name LIKE '%E';
 SELECT * FROM employees WHERE last_name LIKE 'E%' AND last_name LIKE '%E';
 
 SELECT * FROM employees WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%'
-
-
 
