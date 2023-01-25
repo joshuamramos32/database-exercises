7e4bf827c7c33bf94644398de7e2b1827ac50e46
Joshua Ramos <joshua.m.ramos32@gmail.com>
2023-01-25 09:32:11 -0600

    fixing select_exercise

--- select_exercises.sql
+++ select_exercises.sql
@@ -1,3 +1,14 @@
 USE codeup_test_db;
 
-SELECT
+SELECT name AS'Albums by Pink Floyd'FROM WHERE artist_name = 'PINK FLOYD';
+
+SELECT artist_name AS 'Sgt. Peppers Lonely Hearts Club Band'FROM WHERE release_date ='';
+
+SELECT artist_name AS 'Nevermind' FROM WHERE genre = '';
+
+SELECT * FROM albums WHERE release_date > 1990;
+
+SELECT * FROM albums WHERE sales < 20;
+
+SELECT * FROM albums WHERE genre = 'Rock';
+
