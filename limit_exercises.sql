81ec3b6128acb49bbc29c0ec14688a8145160881
Joshua Ramos <joshua.m.ramos32@gmail.com>
2023-01-26 10:51:50 -0600

    maikng changes

--- limit_exercises.sql
+++ limit_exercises.sql
@@ -1,7 +1,8 @@
 USE employees;
 
-SELECT DISTINCT last_name FROM employees WHERE last_name LIKE 'Z%' ORDER BY last_name DESC LIMIT 10;
+SELECT DISTINCT last_name FROM employees ORDER BY last_name DESC LIMIT 10;
 
-SELECT emp_no FROM salaries WHERE salary ORDER BY salary DESC LIMIT 4 OFFSET 46;
+SELECT emp_no FROM salaries ORDER BY salary DESC LIMIT 5 ;
+SELECT emp_no FROM salaries ORDER BY salary DESC LIMIT 5 OFFSET 45;
 
 
