2d69289cdefbf042db8e072fb7ad3d88c720900f
Joshua Ramos <joshua.m.ramos32@gmail.com>
2023-01-30 09:28:26 -0600

    making changes

--- group_by_exercises.sql
+++ group_by_exercises.sql
@@ -2,9 +2,9 @@ USE employees;
 
 SELECT DISTINCT title FROM titles;
 
-SELECT DISTINCT last_name FROM employees WHERE last_name LIKE 'E%E' GROUP BY last_name;
+SELECT DISTINCT last_name FROM employees WHERE last_name LIKE 'E%E' GROUP BY last_name ORDER BY last_name;
 
-SELECT DISTINCT first_name, last_name FROM employees WHERE  last_name LIKE 'E%E';
+SELECT first_name, last_name FROM employees WHERE  last_name LIKE 'E%E'GROUP BY first_name, last_name ORDER BY last_name;
 
 SELECT DISTINCT last_name FROM employees where last_name LIKE '%q%' AND last_name NOT LIKE '%qu%' GROUP BY last_name;
 
