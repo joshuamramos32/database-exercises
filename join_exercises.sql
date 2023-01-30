330e643fac400bc8154fa9b5af500167ca7a6411
Joshua Ramos <joshua.m.ramos32@gmail.com>
2023-01-30 15:55:12 -0600

    making changes

--- join_exercises.sql
+++ join_exercises.sql
@@ -36,15 +36,28 @@ FROM users
 JOIN roles ON users.role_id = roles.id;
 
 USE employees;
-SELECT d.dept_name AS 'Department Name', CONCAT(first_name,',',last_name)AS 'Department Manager'
-FROM departments AS d JOIN dept_manager AS dm ON d.dept_no = dm.dept_no JOIN employees AS e ON dm.emp_no = e.emp_no
-WHERE dm.to_date ='9999-01-01' ORDER BY d.dept_name;
+
+SELECT d.dept_name AS Department_Name, CONCAT(first_name,',',last_name)AS Department_Manager
+FROM departments AS d
+JOIN dept_manager AS dm ON d.dept_no = dm.dept_no
+JOIN employees AS e ON dm.emp_no = e.emp_no
+WHERE dm.to_date ='9999-01-01' AND e.gender = 'F'
+ORDER BY d.dept_name;
 
 SELECT t.title AS Title, count(t.title) AS 'Count'
-FROM titles AS t JOIN dept_emp AS de ON t.emp_no = de.emp_no JOIN departments AS d ON de.dept_no = d.dept_no
-WHERE d.dept_name ='Customer Service' AND t.to_date = '9999-01-01' GROUP BY t.title;
+FROM titles AS t
+JOIN dept_emp AS de ON t.emp_no = de.emp_no
+JOIN departments AS d ON de.dept_no = d.dept_no
+WHERE d.dept_name ='Customer Service'
+AND de.to_date = '9999-01-01' AND t.to_date ='9999-01-01'
+GROUP BY t.title;
 
-SELECT d.dept_name AS 'Department Name', CONCAT(e.first_name,' ',e.last_name)AS 'Name',s.salary AS 'Salary'
-FROM employees AS e JOIN dept_manager AS dm on e.emp_no = dm.emp_no
-JOIN salaries AS s on dm.emp_no = s.emp_no JOIN departments AS d on d.dept_no = dm.dept_no WHERE s.to_date = '9999-01-01' AND dm.to_date = '9999=01-01' ORDER BY d.dept_name;
+
+SELECT d.dept_name AS Department_Name, CONCAT(e.first_name,' ',e.last_name)AS 'Name',s.salary AS 'Salary'
+FROM employees AS e
+JOIN dept_manager AS dm ON e.emp_no = dm.emp_no
+JOIN salaries AS s on dm.emp_no = s.emp_no
+JOIN departments AS d on d.dept_no = dm.dept_no
+WHERE s.to_date = '9999-01-01' AND dm.to_date = '9999=01-01'
+ORDER BY d.dept_name;
 
